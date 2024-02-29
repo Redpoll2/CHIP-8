97d804d59542863a7950061baa594ecad95b7f6c
Timur Vafin <51379496+Redpoll2@users.noreply.github.com>
2024-02-29 23:31:59 +0500

    Add files via upload

---
+++ ChipEight.Runtime/ChipEightInterpreter.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace ChipEight.Runtime
+{
+	public class ChipEightInterpreter
+	{
+		private readonly MemoryStream _memory;
+		private readonly byte[] _registers;
+		private readonly Stack<short> _stack;
+		private readonly unsafe short* _index;
+
+		public ChipEightInterpreter(ChipEightVirtualMachine virtualMachine)
+		{
+			if (virtualMachine is null)
+				throw new ArgumentNullException(nameof(virtualMachine));
+
+			_memory = virtualMachine.Memory;
+
+			_registers = new byte[0xF];
+			_stack = new Stack<short>(0xF);
+		}
+
+		public void Step()
+		{
+			byte low = (byte)_memory.ReadByte();
+			byte high = (byte)_memory.ReadByte();
+
+			// if opcode has not operands
+			if (low == 0x00)
+			{
+				switch (high)
+				{
+					case 0xE0:	// CLS
+						ClearScreen();
+						return;
+
+					case 0xEE:	// RET
+						Return();
+						return;
+				}
+			}
+
+			byte x = (byte)(low & 0xF);
+			byte y = (byte)(high >> 4);
+			short address = (short)((x << 8) | high);
+
+			byte nibble = (byte)(low >> 4);
+
+			switch (nibble)
+			{
+				case 0x0:
+					SystemCall(address);
+					return;
+
+				case 0x1:
+					Jump(address);
+					return;
+
+				case 0x2:
+					Call(address);
+					return;
+
+				case 0x3:
+					SkipIfEquals(x, high);
+					return;
+
+				case 0x4:
+					SkipIfNotEquals(x, high);
+					return;
+
+				case 0x5:
+					SkipIfRegistersEquals(x, y);
+					return;
+
+				case 0x6:
+					SetRegisterValue(x, high);
+					return;
+
+				case 0x7:
+					IncrementRegister(x, high);
+					return;
+			}
+
+			Debug.WriteLine("Invalid opcode received: 0x" + low.ToString("X2") + high.ToString("X2"));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		[SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
+		private void SystemCall(short address)
+		{
+			// dunno. is it useful for the emulator?
+			Debug.WriteLine("SYS " + address.ToString("X3"));
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void ClearScreen()
+		{
+			// ask renderer to clear buffer;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void Return()
+		{
+			if (!_stack.TryPop(out short address))
+				address = 0x1000; // beginning of the program
+
+			_memory.Position = address;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void Jump(short address)
+		{
+			_memory.Position = address;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void Call(short address)
+		{
+			_stack.Push((short)_memory.Position);
+			_memory.Position = address;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SkipIfEquals(byte index, byte value)
+		{
+			if (_registers[index] == value)
+				_memory.Position += 1;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SkipIfNotEquals(byte index, byte value)
+		{
+			if (_registers[index] != value)
+				_memory.Position += 1;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SkipIfRegistersEquals(byte left, byte right)
+		{
+			if (_registers[left] == _registers[right])
+				_memory.Position += 1;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SetRegisterValue(byte index, byte value)
+		{
+			_registers[index] = value;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void IncrementRegister(byte index, byte value)
+		{
+			_registers[index] += value;
+		}
+
+
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void SkipIfRegistersNotEquals(byte left, byte right)
+		{
+			if (_registers[left] != _registers[right])
+				_memory.Position += 1;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private unsafe void SetMemoryIndex(short address)
+		{
+			*_index = address;
+		}
+	}
+}
---
+++ ChipEight.Runtime/ChipEightVirtualMachine.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ChipEight.Runtime
+{
+	public class ChipEightVirtualMachine : IAsyncDisposable, IDisposable
+	{
+		private readonly ChipEightInterpreter _interpreter;
+		private MemoryStream? _memory;
+		private bool _disposed;
+
+		public ChipEightVirtualMachine(Stream? binary = null)
+		{
+			_memory = new MemoryStream(0x1000)
+			{
+				Position = 0x200
+			};
+
+			if (binary is not null)
+			{
+				Debug.WriteLineIf(binary.Length > 0x1000, "Binary stream is bigger than 4096 bytes. The stream will be truncated.");
+
+				binary.CopyTo(_memory);
+
+				// go back to the beggining of the instructions.
+				_memory.Position = 0x200;
+			}
+
+			_interpreter = new ChipEightInterpreter(this);
+		}
+
+		public MemoryStream Memory => _memory ?? throw new ObjectDisposedException(GetType().Name);
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!_disposed)
+			{
+				if (disposing)
+				{
+					_memory?.Dispose();
+				}
+
+				_memory = null;
+				_disposed = true;
+			}
+		}
+
+		protected virtual async ValueTask DisposeAsyncCore()
+		{
+			if (!_disposed)
+			{
+				if (_memory is not null)
+					await _memory.DisposeAsync().ConfigureAwait(false);
+			}
+		}
+
+		public void Run()
+		{
+			while (true)
+			{
+				_interpreter.Step();
+			}
+		}
+
+		public static void Main(string[]? args)
+		{
+			string binaryPath = @"D:\\Maze (alt) [David Winter, 199x].ch8";
+
+			using var binaryStream = new FileStream(binaryPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+			using var virtualMachine = new ChipEightVirtualMachine(binaryStream);
+
+			virtualMachine.Run();
+		}
+
+		~ChipEightVirtualMachine()
+		{
+			Dispose(disposing: false);
+		}
+
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			await DisposeAsyncCore();
+
+			Dispose(disposing: false);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
